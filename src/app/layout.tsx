2e159fa1d6c64fce59b2e6f8ff8b6eb044b76b28
snow6692 <ahmedha258258@gmail.com>
2025-05-04 12:00:50 +0300

    shadcn,clerk added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { ModeToggle } from "./components/ModeToggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ModeToggle />
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }
---
+++ src/generate-program/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GenerateProgramPage() {
+  return <div>GenerateProgramPage</div>;
+}
+
+export default GenerateProgramPage;
